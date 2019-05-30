db22c5143b9f5c8be541e295b542f5d1157b2588
Aurelijus431 <AurelijusST@gmail.com>
2019-05-30 16:09:52 +0200

    Revert "Merge pull request #16 from expo/1.1.1"
    This reverts commit 5fdc7069e8aec4d45f91cc21678487026aca7a24, reversing
    changes made to de7b146f1a0b5ed64d98c1e5a68401fa077a7523.

--- src/base64forImageUriAsync.js
+++ src/base64forImageUriAsync.js
@@ -1,11 +1,11 @@
 // @flow
-import { readAsStringAsync, EncodingType } from 'expo-file-system';
+import { readAsStringAsync, EncodingTypes } from 'expo-file-system';
 import { getSizeAsync } from './ImageUtils';
 
 async function base64forImageUriAsync(uri: string): Promise<Data> {
   const size = await getSizeAsync(uri);
   const data = await readAsStringAsync(uri, {
-    encoding: EncodingType.Base64,
+    encoding: EncodingTypes.Base64,
   });
   return { data, size };
 }
